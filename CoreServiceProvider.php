ce4ff2ef8ed2295b86eef7451762137cf3f385cc
Tuấn Nguyễn <thanhtuancr2011@gmail.com>
2016-09-04 10:14:08 +0700

    update  core

--- CoreServiceProvider.php
+++ CoreServiceProvider.php
@@ -30,10 +30,6 @@ class CoreServiceProvider extends ServiceProvider {
 	    $this->publishes([
 	        __DIR__.'/public' => base_path('public')   
 	    ]);
-
-	    $this->publishes([
-	        __DIR__.'/config' => base_path('config')   
-	    ]);
 	}
 
 	/**
